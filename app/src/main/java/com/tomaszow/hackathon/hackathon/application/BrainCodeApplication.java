30d6c7f6d571b5a762456bd884ddb3b819dee217
Mateusz Męcina <mateusz.mecina@gmail.com>
2016-03-18 23:07:48 +0100

    Login and Register works, delete additional libs

--- app/src/main/java/com/tomaszow/hackathon/hackathon/application/BrainCodeApplication.java
+++ app/src/main/java/com/tomaszow/hackathon/hackathon/application/BrainCodeApplication.java
@@ -8,8 +8,8 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
-import com.tomaszow.hackathon.hackathon.fetcher.LruBitMapCache;
-import com.tomaszow.hackathon.hackathon.fetcher.OkHttpStack;
+import com.parse.Parse;
+import com.tomaszow.hackathon.hackathon.fetcher.Constants;
 
 /**
  * Created by mateusz on 18.03.2016.
@@ -17,8 +17,6 @@ import com.tomaszow.hackathon.hackathon.fetcher.OkHttpStack;
 public class BrainCodeApplication extends Application {
 
     private static final Object TAG = BrainCodeApplication.class.getClass().getName();
-    private RequestQueue mRequestQueue;
-    private ImageLoader mImageLoader;
     private static BrainCodeApplication sBrainCodeApplication;
 
 
@@ -26,39 +24,15 @@ public class BrainCodeApplication extends Application {
     public void onCreate() {
         super.onCreate();
         sBrainCodeApplication = this;
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                        .applicationId(Constants.AppID)
+                        .clientKey(Constants.ClientKey)
+                        .server("https://parseapi.back4app.com")
+                        .build()
+        );
     }
 
     public static synchronized BrainCodeApplication getInstance() { return sBrainCodeApplication; }
 
-    public RequestQueue getRequestQueue() {
-        if (mRequestQueue == null) {
-            mRequestQueue = Volley.newRequestQueue(getApplicationContext(), new OkHttpStack());
-        }
-        return mRequestQueue;
-    }
 
-    public ImageLoader getImageLoader() {
-        getRequestQueue();
-        if (mImageLoader == null) {
-            mImageLoader = new ImageLoader(this.mRequestQueue, new LruBitMapCache());
-        }
-        return this.mImageLoader;
-    }
-
-    public <T> void addToRequestQueue(Request<T> req, String tag) {
-        req.setRetryPolicy(new DefaultRetryPolicy(40 * 1000, 1, 1.0f)); //Request timeout and retry policy
-        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
-        getRequestQueue().add(req);
-    }
-
-    public <T> void addToRequestQueue(Request<T> req) {
-        req.setTag(TAG);
-        getRequestQueue().add(req);
-    }
-
-    public void cancelPendingRequests(Object tag) {
-        if (mRequestQueue != null) {
-            mRequestQueue.cancelAll(tag);
-        }
-    }
 }
