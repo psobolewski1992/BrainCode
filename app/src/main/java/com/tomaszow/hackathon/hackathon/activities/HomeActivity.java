7f690cbce3467b53d43a303d48cad7a4a55a8f90
psobolewski1992 <psobolewski1992@gmail.com>
2016-03-18 22:45:17 +0100

    Home layout

--- app/src/main/java/com/tomaszow/hackathon/hackathon/activities/HomeActivity.java
+++ app/src/main/java/com/tomaszow/hackathon/hackathon/activities/HomeActivity.java
@@ -1,15 +1,24 @@
 package com.tomaszow.hackathon.hackathon.activities;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import com.tomaszow.hackathon.hackathon.R;
 
 public class HomeActivity extends AppCompatActivity {
+    private static final int PROGRESS = 0x1;
 
+    private ProgressBar mProgress;
+    private int mProgressStatus = 0;
+
+    private Handler mHandler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mProgress = (ProgressBar) findViewById(R.id.progressBar);
+
     }
 }
