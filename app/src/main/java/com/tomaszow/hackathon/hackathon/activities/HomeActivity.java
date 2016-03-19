592a6f841aed82386fce3c222f2a7ab8d88d23eb
psobolewski1992 <psobolewski1992@gmail.com>
2016-03-19 05:54:11 +0100

    update activity_map

--- app/src/main/java/com/tomaszow/hackathon/hackathon/activities/HomeActivity.java
+++ app/src/main/java/com/tomaszow/hackathon/hackathon/activities/HomeActivity.java
@@ -9,11 +9,13 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.tomaszow.hackathon.hackathon.R;
+import com.tomaszow.hackathon.hackathon.model.Measurement;
 import com.tomaszow.hackathon.hackathon.services.GPSTracker;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String ID = "Id";
     private static final int PROGRESS = 0x1;
-
+    private String userId;
     private ProgressBar mProgress;
     private int mProgressStatus = 0;
     GPSTracker mGPS;
@@ -21,33 +23,47 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     //private ProgressBar mProgressBar;
 
     private Handler mHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getArguments();
         mProgress = (ProgressBar) findViewById(R.id.progressBar);
         mProgress.setOnClickListener(this);
         getLocation();
 
     }
 
+    private void getArguments() {
+        Intent myHomeIntent = getIntent();
+        userId = myHomeIntent.getStringExtra(ID);
+    }
+
     private void getLocation() {
         mGPS = new GPSTracker(HomeActivity.this);
 
         // check if GPS enabled
-        if(mGPS.canGetLocation()){
+        if (mGPS.canGetLocation()) {
 
             double latitude = mGPS.getLatitude();
             double longitude = mGPS.getLongitude();
-
+            sendCordsToServer(latitude, longitude);
             // \n is for new line
             Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-        }else{
+        } else {
             // can't get location
             // GPS or Network is not enabled
             // Ask user to enable GPS/network in settings
             mGPS.showSettingsAlert();
         }
+    }
+
+    private void sendCordsToServer(double latitude, double longitude) {
+        Measurement newMeasurment = new Measurement();
+        newMeasurment.setLatitude(latitude);
+        newMeasurment.setLongitude(longitude);
+        newMeasurment.saveInBackground();
     }
 
     private void openMapTrackerActivity() {
