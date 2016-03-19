beae777c85d047dc26e9fc2c990f5c1bcff86ddd
psobolewski1992 <psobolewski1992@gmail.com>
2016-03-19 01:38:06 +0100

    Home layout

--- app/src/main/java/com/tomaszow/hackathon/hackathon/activities/HomeActivity.java
+++ app/src/main/java/com/tomaszow/hackathon/hackathon/activities/HomeActivity.java
@@ -1,9 +1,11 @@
 package com.tomaszow.hackathon.hackathon.activities;
 
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.tomaszow.hackathon.hackathon.R;
 
@@ -11,6 +13,7 @@ public class HomeActivity extends AppCompatActivity {
     private static final int PROGRESS = 0x1;
 
     private ProgressBar mProgress;
+    private TextView mNumberOfFriends;
     private int mProgressStatus = 0;
 
     private Handler mHandler = new Handler();
@@ -19,6 +22,9 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         mProgress = (ProgressBar) findViewById(R.id.progressBar);
+        mNumberOfFriends = (TextView) findViewById(R.id.numberOfFriends);
+//        Typeface custom_font = Typeface.createFromAsset(getAssets(), "font.ttf");
+//        mNumberOfFriends.setTypeface(custom_font);
 
     }
 }
